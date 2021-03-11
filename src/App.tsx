132c8005d9eee6cf0a5735956ce6f159d90e4dfc
John <john@trabian.com>
2021-03-10 23:13:10 -0500

    Finish out basic formatted inpus

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { format, isValid } from 'date-fns'
+
+/**
+ * checks if all characters in a string are digits.
+ * @param candidate
+ * @returns boolean
+ */
+const isDigit = (candidate: string) => {
+  return /^\d?$/.test(candidate)
+}
+
+const removeNonDigits = (string: string) => {
+  return string.replace(/\D/g, '')
+}
 
 const App: React.FC = () => {
+  const [moneyValue, setMoneyValue] = useState<string>('')
+
+  const [dateValue, setDateValue] = useState<string>('')
+
+  const [statementVisible, setStatementVisible] = useState<boolean>(false)
+
+  const toggleStatement = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    setStatementVisible(!statementVisible)
+  }
+
+  const changeMoneyValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = removeNonDigits(e.currentTarget.value)
+
+    if (!value) {
+      setMoneyValue('')
+      return
+    }
+
+    const mostRecentChar = value.slice(-1)
+
+    if (isDigit(mostRecentChar)) {
+      const formattedValue = `$${value}`
+
+      setMoneyValue(formattedValue)
+    }
+  }
+
+  const changeDateInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value
+    setDateValue(value)
+  }
+
+  const changeDateValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value
+
+    const mostRecentChar = e.currentTarget.value.slice(-1)
+
+    if (!isDigit(mostRecentChar)) return
+
+    const valueAsDate = new Date(value)
+
+    if (isValid(valueAsDate)) {
+      const formatted = format(valueAsDate, 'yyyy-M-d')
+
+      setDateValue(formatted)
+    } else {
+      alert('Not a valid date')
+    }
+  }
+
   return (
     <Layout>
       <Box>
         <Form>
-          <Input />
-          <Input />
-          <Button />
+          <Input
+            label="Money Input"
+            value={moneyValue}
+            onChange={changeMoneyValue}
+          />
+          <Input
+            style={{ marginTop: '5px' }}
+            label="Date Input"
+            value={dateValue}
+            onChange={changeDateInput}
+            onBlur={changeDateValue}
+          />
+          <Button style={{ marginTop: '10px' }} onClick={toggleStatement}>
+            {statementVisible ? 'Hide the truth' : 'Tell the truth'}
+          </Button>
         </Form>
-        <Box>text</Box>
+        {statementVisible ? (
+          <Box>
+            I will give John {moneyValue} on {dateValue}
+          </Box>
+        ) : null}
       </Box>
     </Layout>
   )
@@ -31,16 +113,35 @@ type InputProps = StyledComponent &
   React.DetailedHTMLProps<
     React.InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
-  >
+  > & { label?: string }
 
-const Input: React.FC<InputProps> = ({ children, ...props }) => {
-  return <input {...props} value="" onChange={() => {}} />
+const Input: React.FC<InputProps> = ({ children, label, ...props }) => {
+  return (
+    <Box
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'baseline',
+      }}
+    >
+      <Box>{label}</Box>
+      <input {...props} />
+    </Box>
+  )
 }
 
-type FormProps = StyledComponent & {}
+type FormProps = StyledComponent &
+  React.DetailedHTMLProps<
+    React.FormHTMLAttributes<HTMLFormElement>,
+    HTMLFormElement
+  > & {}
 
-const Form: React.FC<FormProps> = ({ children, style }) => {
-  return <form style={style}>{children}</form>
+const Form: React.FC<FormProps> = ({ children, ...props }) => {
+  return (
+    <form {...props} style={{ display: 'flex', flexDirection: 'column' }}>
+      {children}
+    </form>
+  )
 }
 
 type LayoutProps = StyledComponent & {}
